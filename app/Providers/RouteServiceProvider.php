a4b88767f134e1afcc04660bd48dff6e3837a880
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-07-25 16:20:30 +0200

    Route changed to /trucks

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/trucks';
 
     /**
      * Define your route model bindings, pattern filters, etc.
