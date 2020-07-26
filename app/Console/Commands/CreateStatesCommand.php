09138f6ed2c0384ab050f6948e60739a72ca8367
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-07-26 20:58:55 +0200

    States added in create_states command

--- app/Console/Commands/CreateStatesCommand.php
+++ app/Console/Commands/CreateStatesCommand.php
@@ -38,8 +38,11 @@ class CreateStatesCommand extends Command
      */
     public function handle()
     {
-        State::create([
-            'state' => "Serbia" 
-        ]);
+        $states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oklahoma', 'Pennsylvania', 'Rhode Island', 'South Carolina','South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+        foreach ($states as $state) {
+            State::create([
+                'state' => $state
+            ]);
+        }
     }
 }
