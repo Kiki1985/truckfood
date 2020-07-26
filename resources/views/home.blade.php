e0c7a5485402932d0f9c33d8aa4a303745653025
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-07-26 10:34:58 +0200

    trucks table and realationship added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,10 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    @foreach(auth()->user()->trucks as $truck)
+                        {{$truck->name}}
+                    @endforeach
                 </div>
             </div>
         </div>
