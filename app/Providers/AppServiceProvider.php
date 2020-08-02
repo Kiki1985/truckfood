53482456ddca4b1833d35c4724f927a0d316d021
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-08-02 18:41:45 +0200

    $trucks to home blade from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer("home", function ($view) {
+            $trucks = \App\Truck::all();
+            $view->with(compact("trucks"));
+        });
     }
 }
